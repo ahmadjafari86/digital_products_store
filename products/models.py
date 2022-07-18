a9607bcebc1b668e9db4cf2a449d5b2bc83ca41d
ahmadDev <ahmadjafari6050@gmail.com>
2022-07-18 22:49:11 +0430

    models are done

--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
-    categories = models.ManyToManyField(_('Category'), verbose_name=_('categories'), blank=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
